915c30ee874467705e9520c09483e9062ca1c04b
davidpierce24 <pie2ce@gmail.com>
2022-06-06 17:49:07 -0400

    collections

---
+++ CollectionsPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿// Three basic arrays
+int [] arr1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+string [] arr2 = {"Tim", "Martin", "Nikki", "Sara"};
+
+bool [] arr3 = new bool[10];
+for(int i = 0; i < 10; i++)
+{
+    if(i%2 == 1)
+    {
+        arr3[i] = false;
+    }
+    else
+    {
+        arr3[i] = true;
+    }
+    System.Console.WriteLine(arr3[i]);
+}
+
+
+
+// List of flavors
+
+
+
+// User info dictionary
