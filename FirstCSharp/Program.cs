3e762982d1313616adffaa21833fa7d98c606504
davidpierce24 <pie2ce@gmail.com>
2022-06-06 12:10:01 -0400

    updates

--- FirstCSharp/Program.cs
+++ FirstCSharp/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World! My first project");
+
+
+// javascript
+// var name = "hello";
+
+// python
+// name = "hello"
+
+// C#
+string name = "hello";
+// we have to declare data types at the start of EVERY variable
+
+
+// JS function
+//  function functionName() {}
+
+// C# function
+// Need to specify what will be returned by the function
+static void functionName()
+{
+    // void is the term we use when there is No return statement
+}
+
+static string Hello()
+{
+    return "Hello";
+}
+
+static int Num()
+{
+    return 17;
+}
