9b711e3521d5b0c0d82090921166e4828f66b2c1
davidpierce24 <pie2ce@gmail.com>
2022-06-07 11:23:39 -0400

    class lecture

---
+++ classlecture1/Animal.cs
@@ -0,0 +1,14 @@
+class Animal
+{
+    // establish the attributes of the class
+    string Species;
+    double Weight;
+    string Diet;
+
+    Animal(string species, double weight, string diet)
+    {
+        Species = species;
+        Weight = weight;
+        Diet = diet;
+    }
+}
