1f4a1d4637107bf0e44c263a8aee954896be73d7
davidpierce24 <pie2ce@gmail.com>
2022-06-07 11:42:10 -0400

    updates

--- classlecture1/Animal.cs
+++ classlecture1/Animal.cs
@@ -1,14 +1,34 @@
 class Animal
 {
     // establish the attributes of the class
-    string Species;
-    double Weight;
-    string Diet;
+    // words used to describe an item
+    public string Species;
+    public double Weight;
+    public string Diet;
+    public bool isFriendly;
 
-    Animal(string species, double weight, string diet)
+    // this is our constructor
+    public Animal(string species, double weight, string diet)
     {
         Species = species;
         Weight = weight;
         Diet = diet;
+        isFriendly = false;
+    }
+
+    public Animal(string species, double weight, string diet, bool isFr)
+    {
+        Species = species;
+        Weight = weight;
+        Diet = diet;
+        isFriendly = isFr;
+    }
+
+    // second constructor with only 2 attributes that autofills the last one
+    public Animal(string species, double weight)
+    {
+        Species = species;
+        Weight = weight;
+        Diet = "Ominvore";
     }
 }
--- classlecture1/Program.cs
+++ classlecture1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+// first instance of an animal
+Animal Dog = new Animal("Dog", 60.3, "Ominvore", true);
+Animal Cat = new Animal("Cat", 15.5, "Ominvore", true);
+Animal Bear = new Animal("Bear", 160.3, "Ominvore");
+Animal Coyote = new Animal("Coyote", 56.2);
+
+Console.WriteLine(Dog.Diet);
+Console.WriteLine(Bear.isFriendly);
+Console.WriteLine(Dog.isFriendly);
+Console.WriteLine(Coyote.Diet);
