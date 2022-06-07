acf3150c4e5d47d347216b6982e7646d1be0b66d
davidpierce24 <pie2ce@gmail.com>
2022-06-06 23:08:03 -0400

    updates

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,20 @@
+﻿List<object> empty = new List<object>();
+
+empty.Add(7);
+empty.Add(28);
+empty.Add(-1);
+empty.Add(true);
+empty.Add("chair");
+
+int open = 0;
+
+foreach (var x in empty)
+{
+    System.Console.WriteLine(x);
+    if (x is int)
+    {
+        open += (int)x;
+    }
+}
+
+    System.Console.WriteLine(open);
