b280f30df4fa729f6fc02398012cf7f44cbec5c8
davidpierce24 <pie2ce@gmail.com>
2022-06-10 12:33:04 -0400

    updates

--- SessionLecture/Controllers/HomeController.cs
+++ SessionLecture/Controllers/HomeController.cs
@@ -16,12 +16,34 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        // create a session variable
-        HttpContext.Session.SetString("User", "Ichigo");
-        // to get the data
-        string? user = HttpContext.Session.GetString("User");
-        Console.WriteLine(user);
+        // // create a session variable
+        // HttpContext.Session.SetString("User", "Ichigo");
+        // // to get the data
+        // string? user = HttpContext.Session.GetString("User");
+        // Console.WriteLine(user);
+        // // HttpContext.Session.Clear();
+        // // we can check if there's anythin in session and do stuff accordingly
+        if (HttpContext.Session.GetInt32("Sum") == null)
+        {
+            HttpContext.Session.SetInt32("Sum", 0);
+        }
         return View();
+    }
+
+    [HttpPost("setName")]
+    public IActionResult setName(string Name, int Num)
+    {
+        HttpContext.Session.SetString("User", Name);
+        int? original = HttpContext.Session.GetInt32("Sum");
+        HttpContext.Session.SetInt32("Sum", (int)original + Num);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet("delete")]
+    public IActionResult deleteSession()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
     }
 
     public IActionResult Privacy()
