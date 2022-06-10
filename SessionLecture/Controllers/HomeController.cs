817584151224174ffe224674481e9a53fd1ca641
davidpierce24 <pie2ce@gmail.com>
2022-06-10 11:21:20 -0400

    session lecture

---
+++ SessionLecture/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SessionLecture.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace SessionLecture.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        // create a session variable
+        HttpContext.Session.SetString("User", "Ichigo");
+        // to get the data
+        string? user = HttpContext.Session.GetString("User");
+        Console.WriteLine(user);
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
