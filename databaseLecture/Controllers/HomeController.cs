ef8ab41c483f64bc8c53d1d7dc0ccc0200d8d607
davidpierce24 <pie2ce@gmail.com>
2022-06-14 11:33:26 -0400

    updates

--- databaseLecture/Controllers/HomeController.cs
+++ databaseLecture/Controllers/HomeController.cs
@@ -13,7 +13,6 @@ public class HomeController : Controller
     {
         _logger = logger;
         // this establishes the connection to our database
-        
         _context = context;
     }
 
