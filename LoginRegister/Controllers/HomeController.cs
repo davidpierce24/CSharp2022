2cc0895cdd8bd0279f4a6c1395c9e6f89e000576
davidpierce24 <pie2ce@gmail.com>
2022-06-15 14:45:46 -0400

    updates

--- LoginRegister/Controllers/HomeController.cs
+++ LoginRegister/Controllers/HomeController.cs
@@ -17,11 +17,69 @@ public class HomeController : Controller
         _context = context;
     }
 
+    // Route to the home login / register page
     public IActionResult Index()
     {
+        HttpContext.Session.Clear();
         return View();
     }
 
+    // route to process a user registration
+    [HttpPost("user/register")]
+    public IActionResult Register(User newUser)
+    {
+        if(ModelState.IsValid){
+            // validation for unique email
+            if(_context.Users.Any(d => d.Email == newUser.Email)){
+                // user email already in db
+                ModelState.AddModelError("Email", "Email is already in use!");
+                return View("Index");
+            }
+            // validation for unique username
+            if(_context.Users.Any(d => d.Username == newUser.Username)){
+                // username already in db
+                ModelState.AddModelError("Username", "Username is already in use!");
+                return View("Index");
+            }
+            // password hasing
+            PasswordHasher<User> Hasher = new PasswordHasher<User>();
+            newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+            _context.Add(newUser);
+            _context.SaveChanges();
+            HttpContext.Session.SetInt32("user", newUser.UserId);
+            return RedirectToAction("Success");
+        } else {
+            return View("Index");
+        }
+    }
+
+
+    // route to process a user login
+    [HttpPost("user/login")]
+    public IActionResult Login(LogUser loginUser)
+    {
+        if(ModelState.IsValid){
+            return RedirectToAction("Success");
+        } else {
+            return View("Index");
+        }
+    }
+
+
+    // route for successful login/register page
+    [HttpGet("success")]
+    public IActionResult Success()
+    {
+        if(HttpContext.Session.GetInt32("user") == null){
+            return RedirectToAction("Index");
+        }
+        User loggedInUser = _context.Users.FirstOrDefault(x => x.UserId == HttpContext.Session.GetInt32("user"));
+        return View();
+    }
+
+
+
+    // route for a privacy meme
     public IActionResult Privacy()
     {
         return View();
