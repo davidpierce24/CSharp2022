525b39bf40e4419a5009c1ce1d765eec67b81f07
davidpierce24 <pie2ce@gmail.com>
2022-06-15 13:45:16 -0400

    updates

--- LoginRegister/Models/User.cs
+++ LoginRegister/Models/User.cs
@@ -6,5 +6,25 @@ namespace LoginRegister.Models;
 
 public class User
 {
-
+    [Key]
+    public int UserId {get; set;}
+    [Required]
+    public string FirstName {get; set;}
+    [Required]
+    public string LastName {get; set;}
+    [Required]
+    public string Username {get; set;}
+    [Required]
+    [EmailAddress]
+    public string Email {get; set;}
+    [Required]
+    [MinLength(8)]
+    [DataType(DataType.Password)]
+    public string Password {get; set;}
+    [NotMapped]
+    [Compare("Password")]
+    [DataType(DataType.Password)]
+    public string ConfrimPassword {get; set;}
+    public DateTime CreatedAt {get; set;} = DateTime.Now;
+    public DateTime UpdatedAt {get; set;} = DateTime.Now;
 }
