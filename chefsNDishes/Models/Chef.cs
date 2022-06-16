1daf17b2c69bf677b68e6a7a2f7d3167f5c3ac7e
davidpierce24 <pie2ce@gmail.com>
2022-06-16 18:00:04 -0400

    updates

--- chefsNDishes/Models/Chef.cs
+++ chefsNDishes/Models/Chef.cs
@@ -8,11 +8,41 @@ public class Chef
 {
     [Key]
     public int ChefId {get; set;}
+    [Required]
     public string FirstName {get; set;}
+    [Required]
     public string LastName {get; set;}
+    [Required]
+    [DataType(DataType.Date)]
+    [Past]
+    [Over18]
     public DateTime DOB {get; set;}
     public DateTime CreatedAt {get; set;} = DateTime.Now;
     public DateTime UpdatedAt {get; set;} = DateTime.Now;
     public List<Dish> ChefDishes {get; set;} = new List<Dish>();
 
+}
+
+public class PastAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (((DateTime)value) >= DateTime.Now)
+        {
+            return new ValidationResult("You can't be from the future!");
+        }
+        return ValidationResult.Success;
+    }
+}
+
+public class Over18Attribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (DateTime.Now.Year - ((DateTime)value).Year < 18)
+        {
+            return new ValidationResult("Too Young!");
+        }
+        return ValidationResult.Success;
+    }
 }
--- chefsNDishes/Models/Dish.cs
+++ chefsNDishes/Models/Dish.cs
@@ -8,12 +8,19 @@ public class Dish
 {
     [Key]
     public int DishId {get; set;}
+    [Required]
     public string Name {get; set;}
+    [Required]
+    [Range(1,int.MaxValue)]
     public int Calories {get; set;}
+    [Required]
     public string Description {get; set;}
+    [Required]
+    [Range(1, 5)]
     public int Tastiness {get; set;}
     public DateTime CreatedAt {get; set;} = DateTime.Now;
     public DateTime UpdatedAt {get; set;} = DateTime.Now;
+    [Required]
     public int ChefId {get; set;}
     public Chef? Chef {get; set;}
 }
