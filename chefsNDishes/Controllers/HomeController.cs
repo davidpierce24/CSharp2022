4751fa4f4d5ed60fc63f95831fc86a81b7594378
davidpierce24 <pie2ce@gmail.com>
2022-06-16 23:57:52 -0400

    updates

--- chefsNDishes/Controllers/HomeController.cs
+++ chefsNDishes/Controllers/HomeController.cs
@@ -17,18 +17,43 @@ public class HomeController : Controller
         _context = context;
     }
 
+    // route to render home page with all chefs
     public IActionResult Index()
     {
         ViewBag.Chefs = _context.Chefs.ToList();
         return View();
     }
 
+    // route to render all dishes
     [HttpGet("dishes/show")]
     public IActionResult Dishes()
     {
         ViewBag.Dishes = _context.Dishes.ToList();
         return View();
     }
+
+    // route to display add a chef form
+    [HttpGet("chef/add")]
+    public IActionResult AddChef()
+    {
+        return View();
+    }
+
+    // route to process added chef
+    [HttpPost("chef/add/process")]
+    public IActionResult ProcessChef(Chef newChef)
+    {
+        if(ModelState.IsValid){
+            _context.Add(newChef);
+            _context.SaveChanges();
+            ViewBag.Chefs = _context.Chefs.ToList();
+            return RedirectToAction("Index");
+        } else {
+            return View("AddChef");
+        }
+    }
+
+    // route to display added
 
     public IActionResult Privacy()
     {
