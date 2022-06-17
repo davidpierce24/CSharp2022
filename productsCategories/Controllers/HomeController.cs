450037d7aacc696cc4e9ee4621ef0054e571b879
davidpierce24 <pie2ce@gmail.com>
2022-06-17 15:14:54 -0400

    updates

--- productsCategories/Controllers/HomeController.cs
+++ productsCategories/Controllers/HomeController.cs
@@ -18,10 +18,58 @@ public class HomeController : Controller
         _context = context;
     }
 
+    // route to render the homepage with products
     public IActionResult Index()
     {
+        ViewBag.Products = _context.Products.ToList();
         return View();
     }
+
+    // route to process adding a product
+    [HttpPost("product/add")]
+    public IActionResult AddProduct(Product newProduct)
+    {
+        if(ModelState.IsValid){
+            _context.Add(newProduct);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        } else {
+            ViewBag.Products = _context.Products.ToList();
+            return View("Index");
+        }
+    }
+
+    // route to render the categories page
+    [HttpGet("category")]
+    public IActionResult Category()
+    {
+        ViewBag.Categories = _context.Categories.ToList();
+        return View();
+    }
+
+    // route to process adding a category
+    [HttpPost("category/add")]
+    public IActionResult AddCategory(Category newCategory)
+    {
+        if(ModelState.IsValid){
+            _context.Add(newCategory);
+            _context.SaveChanges();
+            return RedirectToAction("Category");
+        } else {
+            ViewBag.Categories = _context.Categories.ToList();
+            return View("Category");
+        }
+    }
+
+    // route to render a specific product page
+
+    // route to process adding a category to a product
+
+    // route to render a specific category page
+
+    // route to process adding a product to a category
+
+
 
     public IActionResult Privacy()
     {
