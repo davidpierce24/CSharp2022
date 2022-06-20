da5f11bda20c5ea8ef652e2846dfaa8d01ea6508
davidpierce24 <pie2ce@gmail.com>
2022-06-20 14:07:13 -0400

    updates

--- weddingPlanner/Controllers/HomeController.cs
+++ weddingPlanner/Controllers/HomeController.cs
@@ -1,22 +1,30 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using weddingPlanner.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace weddingPlanner.Controllers;
 
 public class HomeController : Controller
 {
+    private MyContext _context;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, MyContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
+    // Route to render home login page
     public IActionResult Index()
     {
         return View();
     }
+
+
 
     public IActionResult Privacy()
     {
