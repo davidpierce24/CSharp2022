ecf2cda4a0c91a5095a91575168c4ffd9ddddfd1
davidpierce24 <pie2ce@gmail.com>
2022-06-27 10:41:09 -0400

    updates

--- Algos/w4/d1/algos.js
+++ Algos/w4/d1/algos.js
@@ -27,7 +27,8 @@ class Stack{
 
     Peek()
     {
-        //Code me!
+        if(!this.top) return;
+        return this.top.data;
     }
 
     Pop()
@@ -40,7 +41,7 @@ class Stack{
 
     IsEmpty()
     {
-        //Code me!
+        return this.top == null;
     }
 
     Log()
@@ -56,15 +57,15 @@ class Stack{
 
 
 let stack=new Stack();
-// console.log(stack.IsEmpty()); /* Expected: False */
+console.log(stack.IsEmpty()); /* Expected: true */
 stack.Push(10);
 stack.Push(20);
 stack.Push(30);
 stack.Push(40);
 stack.Log(); /* Expected: 40->30->20->10-> */
 console.log(stack.Pop()); /* Expected: 40 */
-// console.log(stack.Peek()); /* Expected: 30 */
-// stack.Pop(); /* 30 */
-// stack.Pop(); /* 20 */
-// stack.Pop(); /* 10 */
-// console.log(stack.IsEmpty()); /* Expected: true */
+console.log(stack.Peek()); /* Expected: 30 */
+console.log(stack.Pop()); /* Expected: 30 */
+console.log(stack.Pop()); /* Expected: 20 */
+console.log(stack.Pop()); /* Expected: 10 */
+console.log(stack.IsEmpty()); /* Expected: true */
